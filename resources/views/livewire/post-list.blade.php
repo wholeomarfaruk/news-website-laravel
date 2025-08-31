1d7bdf4ec0fc226d4e62bac07ffe070be81bc6d0
wholeomarfaruk <wholeomarfaruk@gmail.com>
2025-09-01 05:13:29 +0600

    update

--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -191,10 +191,10 @@
                                             <div class="flex items-center gap-2">
 
                                                 @if ($post->status === 'published')
-                                                    {{-- <a target="_blank"  href="{{route('singlepost',$post->slug)}}"
+                                                    <a target="_blank"  href="{{route('post.show',['category'=>$post->category->name,'slug'=>$post->slug])}}"
                                                         class="inline-flex items-center gap-2 rounded-lg bg-white px-4 py-3 text-sm font-medium text-gray-700 shadow-theme-xs ring-1 ring-gray-300 transition hover:bg-gray-50 dark:bg-gray-800 dark:text-gray-400 dark:ring-gray-700 dark:hover:bg-white">
                                                         Show
-                                                    </a> --}}
+                                                    </a>
                                                 @elseif ($post->status === 'draft')
                                                     {{-- <button wire:click="openViewModal({{ $post->id }})"
                                                         class="inline-flex items-center gap-2 rounded-lg bg-white px-4 py-3 text-sm font-medium text-gray-700 shadow-theme-xs ring-1 ring-gray-300 transition hover:bg-gray-50 dark:bg-gray-800 dark:text-gray-400 dark:ring-gray-700 dark:hover:bg-white">
