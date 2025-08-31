0ed9b722049586f1e8ca0fbf15feb0a48c0173f8
wholeomarfaruk <wholeomarfaruk@gmail.com>
2025-09-01 05:23:13 +0600

    update

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(){
+        $posts=Post::sum('views');
         return view('admin.dashboard');
     }
 }
