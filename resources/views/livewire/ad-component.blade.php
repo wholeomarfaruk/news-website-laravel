1696f386ff50080b6a1ecb9543fe165a42e537d6
wholeomarfaruk <wholeomarfaruk@gmail.com>
2025-11-06 04:09:09 +0600

    update

--- resources/views/livewire/ad-component.blade.php
+++ resources/views/livewire/ad-component.blade.php
@@ -4,7 +4,9 @@
 <div>
     @if ($ad)
     <div class="ad bg-dark bg-opacity-50 d-flex justify-content-center align-items-center mb-2">
+        <a href="{{ $ad->link }}" target="_blank">
     <img src="{{ $ad->image }}" class="w-100" alt="">
+    </a>
     </div>
     @endif
 </div>
