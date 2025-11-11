ed1b47e32947c837cbfa3fcc052c8ea40a647ad8
wholeomarfaruk <wholeomarfaruk@gmail.com>
2025-11-12 02:08:15 +0600

    update adcomponent

--- resources/views/livewire/ad-component.blade.php
+++ resources/views/livewire/ad-component.blade.php
@@ -3,7 +3,7 @@
 
 <div>
     @if ($ad)
-    <div class="ad bg-dark bg-opacity-50 d-flex justify-content-center align-items-center mb-2">
+    <div class="ad d-flex justify-content-center align-items-center mb-2">
         <a href="{{ $ad->link }}" target="_blank">
     <img src="{{ $ad->image }}" class="w-100" alt="">
     </a>
